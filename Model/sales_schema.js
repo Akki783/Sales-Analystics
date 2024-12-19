f6099f31e9445a80048f75d865c595d4369fbbb1
Akki783 <platformsupport@dove-soft.com>
2024-12-19 12:56:17 +0530

    update : change schema store code part

--- Model/sales_schema.js
+++ Model/sales_schema.js
@@ -1,88 +1,105 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const storeSchema = new mongoose.Schema({
-    Region: {
+  Region: {
+    type: String,
+    required: true,
+  },
+  "Store Code": [
+    {
+      type: String,
+      required: true
+    }
+  ],
+  YTD: {
+    "Store Age": {
+      type: String,
+    },
+    "New Customer": {
+      type: Number,
+    },
+    "New Customer Revenue": {
+      type: Number,
+    },
+    "Repeat Customer": {
+      type: Number,
+    },
+    "Repeat Customer Revenue": {
+      type: Number,
+    },
+  },
+  MTD: [
+    {
+      Month: {
         type: String,
-        required: true,
+      },
+      "New Customer": {
+        type: Number,
+      },
+      "New Customer Revenue": {
+        type: Number,
+      },
+      "Repeat Customer": {
+        type: Number,
+      },
+      "Repeat Customer Revenue": {
+        type: Number,
+      },
     },
-    "Store Code": {
+  ],
+  "Opportunity Birthday": [
+    {
+      "Total Count": {
+        type: Number,
+      },
+      "Revenue Potential": {
+        type: Number,
+      },
+      "Redeemed Count": {
+        type: Number,
+      },
+      "Sale Achieved": {
+        type: Number,
+      },
+      "Ach %": {
         type: String,
+        default: function () {
+          return this["Total Count"] > 0
+            ? ((this["Redeemed Count"] / this["Total Count"]) * 100).toFixed(
+                2
+              ) + "%"
+            : "0%";
+        },
+      },
     },
-    YTD: {
-        "Store Age": {
-            type: String,
+  ],
+  "Opportunity Anniversary": [
+    {
+      "Total Count": {
+        type: Number,
+      },
+      "Revenue Potential": {
+        type: Number,
+      },
+      "Redeemed Count": {
+        type: Number,
+      },
+      "Sale Achieved": {
+        type: Number,
+      },
+      "Ach %": {
+        type: String,
+        default: function () {
+          return this["Total Count"] > 0
+            ? ((this["Redeemed Count"] / this["Total Count"]) * 100).toFixed(
+                2
+              ) + "%"
+            : "0%";
         },
-        "New Customer": {
-            type: Number,
-        },
-        "New Customer Revenue": {
-            type: Number,
-        },
-        "Repeat Customer": {
-            type: Number,
-        },
-        "Repeat Customer Revenue": {
-            type: Number,
-        },
+      },
     },
-    MTD: [{
-        Month: {
-            type: String,
-        },
-        "New Customer": {
-            type: Number,
-        },
-        "New Customer Revenue": {
-            type: Number,
-        },
-        "Repeat Customer": {
-            type: Number,
-        },
-        "Repeat Customer Revenue": {
-            type: Number,
-        },
-    }],
-    "Opportunity Birthday": [{
-        "Total Count": {
-            type: Number,
-        },
-        "Revenue Potential": {
-            type: Number,
-        },
-        "Redeemed Count": {
-            type: Number,
-        },
-        "Sale Achieved": {
-            type: Number,
-        },
-        "Ach %": {
-            type: String,
-            default: function() {
-                return this["Total Count"] > 0 ? ((this["Redeemed Count"] / this["Total Count"]) * 100).toFixed(2) + '%' : '0%';
-            }
-        },
-    }],
-    "Opportunity Anniversary": [{
-        "Total Count": {
-            type: Number,
-        },
-        "Revenue Potential": {
-            type: Number,
-        },
-        "Redeemed Count": {
-            type: Number,
-        },
-        "Sale Achieved": {
-            type: Number,
-        },
-        "Ach %": {
-            type: String,
-            default: function() {
-                return this["Total Count"] > 0 ? ((this["Redeemed Count"] / this["Total Count"]) * 100).toFixed(2) + '%' : '0%';
-            }
-        },
-    }],
+  ],
 });
 
-const Store = mongoose.model('Store', storeSchema);
+const Store = mongoose.model("Store", storeSchema);
 module.exports = Store;
